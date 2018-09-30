f77bb45373ca306d95923c50488e6c07a7eaa246
USUARIO <USUARIO@Pedro-PC>
2018-09-29 23:37:30 -0500

    Version. 0.0.1 #Mijina Aqui esta todo solo falta el OAD si puede acolite con eso #Mija Ya declare los metodos del OAD

--- src/ec/edu/ups/objetoaccesodatos/OAD.java
+++ src/ec/edu/ups/objetoaccesodatos/OAD.java
@@ -5,6 +5,11 @@
  */
 package ec.edu.ups.objetoaccesodatos;
 
+import ec.edu.ups.entidades.Pelicula;
+import ec.edu.ups.entidades.PeliculaVista;
+import ec.edu.ups.entidades.Usuario;
+import java.util.List;
+
 /**
  *
  * @author Pedro Bermeo
@@ -13,5 +18,69 @@ public class OAD {
 
     public OAD() {
     }
+    
+    public boolean insertarUsuario(Usuario u){
+        
+        return false;
+    }
+    
+    public boolean modificarUsuario(Usuario u){
+        
+        return false;
+    }
+    
+    public boolean eliminarUsuario(int id){
+        
+        return false;
+    }
+    
+    public Usuario buscarUsuario(int cedula){
+        
+        return null;
+    }
+    
+    public List<Usuario>listarUsuarios(){
+        
+        return null;
+    }
+    
+    public boolean insertarPelicula(Pelicula p){
+        return false;
+    }
+    
+    
+    public boolean modificarPelicula(Pelicula p){
+        return false;
+    }
+    
+    
+    public boolean eliminarPelicula(int id){
+        return false;
+    }
+    
+    
+    public Pelicula buscarPelicula(String nombre){
+        return null;
+    }
+    
+    public List<Pelicula>listarPelicula(){
+        return null;
+    }
+    
+    public boolean insertarPeliculaVista(PeliculaVista pv){
+        return false;
+    }
+ 
+    public boolean modificarPeliculaVista(PeliculaVista pv){
+        return false;
+    }
+    public boolean eliminarPeliculaVista(int id){
+        return false;
+    }
+    
+    public List<PeliculaVista> listarPeliculaVista(){
+        return null;
+    }
+    
     
 }
